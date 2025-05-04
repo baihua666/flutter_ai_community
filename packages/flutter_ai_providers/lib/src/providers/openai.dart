7af95024003e65a2336bfdfec5f1b888431ed180
tubao <tubao1985@163.com>
2025-05-04 17:59:31 +0800

    增加系统提示词

--- packages/flutter_ai_providers/lib/src/providers/openai.dart
+++ packages/flutter_ai_providers/lib/src/providers/openai.dart
@@ -137,6 +137,11 @@ class OpenAIProvider extends LlmProvider with ChangeNotifier {
           return ChatCompletionMessage.assistant(
             content: message.text ?? '',
           );
+
+        case MessageOrigin.system:
+          return ChatCompletionMessage.system(
+            content: message.text ?? '',
+          );
       }
     }).toList(growable: false);
   }
